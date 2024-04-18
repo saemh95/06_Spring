139c6b32daa4c66e76f108b472f19b025316748b
saemh95 <saem_hong_kr@hotmail.com>
2024-04-18 17:30:55 +0900

    240418-final

---
+++ BoardProject-boot/src/main/java/edu/kh/project/common/config/FilterConfig.java
@@ -0,0 +1,30 @@
+package edu.kh.project.common.config;
+
+import java.util.Arrays;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import edu.kh.project.common.filter.LoginFilter;
+
+@Configuration
+public class FilterConfig {
+
+	@Bean
+	public FilterRegistrationBean<LoginFilter> loginFilter() {
+		
+		FilterRegistrationBean<LoginFilter> filter = new FilterRegistrationBean<>();
+		filter.setFilter(new LoginFilter());
+		
+		String[] filteringURL = {"/myPage/*"};
+		
+		filter.setUrlPatterns(Arrays.asList(filteringURL));
+		
+		filter.setName("loginFilter");
+		filter.setOrder(1);
+		
+		return filter;
+	}
+	
+}
--- BoardProject-boot/src/main/java/edu/kh/project/main/controller/MainController.java
+++ BoardProject-boot/src/main/java/edu/kh/project/main/controller/MainController.java
@@ -2,6 +2,7 @@ package edu.kh.project.main.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
 @Controller
@@ -10,6 +11,15 @@ public class MainController {
 	@GetMapping("/")
 	public String mainPage() {
 		return "common/main";
+	}
+	
+	@GetMapping("/loginError")
+	public String loginError(RedirectAttributes ra) {
+		
+		ra.addFlashAttribute("message", "Please login first");
+		
+		
+		return "redirect:/";
 	}
 	
 }
